cce98efa42b75d26effba7a614a9e40fcf7c34df
Jose Manuel <78752231+JoseMan981@users.noreply.github.com>
2022-01-18 17:09:18 -0600

    Revert "users_groups_models"
    This reverts commit c4970ea66292c8c6397151874934b47e7dbf9217.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGroupsTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateGroupsTable extends Migration
      */
     public function up()
     {
-        Schema::create('groups', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
-            $table->string('Name');
-            $table->integer('Number_of_members')->unsigned();
-            $table->string('Agency_Id');
-            $table->string('Agency_Name');
-            $table->string('AMBT_Id');
-
         });
     }
 
@@ -32,6 +31,6 @@ class CreateGroupsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('groups');
+        Schema::dropIfExists('users');
     }
 }
