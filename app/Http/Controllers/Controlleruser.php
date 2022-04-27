4a34bfcd57b8cc3c25ec797cd0898f22d754d7cc
Jose Manuel <78752231+JoseMan981@users.noreply.github.com>
2022-04-27 15:10:29 -0500

    test pryeba

--- app/Http/Controllers/Controlleruser.php
+++ app/Http/Controllers/Controlleruser.php
@@ -6,12 +6,5 @@ use Illuminate\Http\Request;
 
 class Controlleruser extends Controller
 {
-    public function FunctionName(Request $request)
-    {
-        //return response($request)->json();
-    }
-    public function test1(Request $var = null)
-    {
-        return response()->json($data, 200, $headers);
-    }
+    
 }
